e97e8b32b357ff3fa7a42d0a5eaf49279631faf3
growingtree-code <tjsruddks12@gmail.com>
2022-01-01 20:20:17 +0900

    retry commit

---
+++ src/LottosHL.java
@@ -0,0 +1,17 @@
+class LottosHL {
+        int[] lottos = {1,2,3,4,5,6};//구매한 로또번호
+        int[] win_nums = {1,2,3,4,5,6};//당첨번호
+
+        public int[] solution(int[] lottos, int[] win_nums) {
+            for(int i = 0;i< lottos.length; i++){
+                //일치하는지 확인
+
+            }
+
+
+            int[] answer = {};
+
+            return answer;
+        }
+    }
+
