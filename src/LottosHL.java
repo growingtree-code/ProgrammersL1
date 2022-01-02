f48ed9671b5a7f841460d55a73a6730f1b1e0539
growingtree-code <tjsruddks12@gmail.com>
2022-01-02 22:43:59 +0900

    test

--- src/LottosHL.java
+++ src/LottosHL.java
@@ -1,17 +1,26 @@
 class LottosHL {
-        int[] lottos = {1,2,3,4,5,6};//구매한 로또번호
-        int[] win_nums = {1,2,3,4,5,6};//당첨번호
+
 
         public int[] solution(int[] lottos, int[] win_nums) {
-            for(int i = 0;i< lottos.length; i++){
-                //일치하는지 확인
 
-            }
 
 
             int[] answer = {};
 
             return answer;
         }
+
+    public static void main(String[] args) {
+        int[] lottos = {1,2,3,4,5,6};//구매한 로또번호
+        int[] win_nums = {1,2,3,4,5,6};//당첨번호
+        for(int i = 0;i< lottos.length; i++){
+            //일치하는지 확인
+            if(lottos[i] == win_nums[i]){
+                System.out.println("lottos["+i+"] == win_nums["+i+"]");
+
+            }
+        }
+    }
+
     }
 
